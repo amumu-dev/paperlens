db5f1fc41cbb9f2f98f9d765ced9867380a78b75
xlvector <xlvector@gmail.com>
2011-07-19 05:16:28 +0000

    using author rank information

--- recommendation/itemsim/paper_sim_by_content.py
+++ recommendation/itemsim/paper_sim_by_content.py
@@ -7,40 +7,39 @@ cursor = connection.cursor()
 simTable = dict()
 
 
-for paper_mod in range(5):
-    for author_mod in range(10):
-        cursor.execute("select paper_id,author_id from paper_author where author_id%10=" + str(author_mod) + " order by author_id;")
+for author_mod in range(10):
+    cursor.execute("select paper_id,author_id from paper_author where rank<2 order by author_id;")
 
-        numrows = int(cursor.rowcount)
-        print paper_mod, author_mod, numrows
+    numrows = int(cursor.rowcount)
+    print paper_mod, author_mod, numrows
 
-        prev_author = -1
-        papers = []
-        for k in range(numrows):
-            row = cursor.fetchone()
-            author_id = row[1]
-            paper_id = row[0]
-            if prev_author != author_id:
-                if len(papers) < 50:
-                    for i in papers:
-                        if i not in simTable:
-                            simTable[i] = dict()
-                        if i % 5 != paper_mod:
+    prev_author = -1
+    papers = []
+    for k in range(numrows):
+        row = cursor.fetchone()
+        author_id = row[1]
+        paper_id = row[0]
+        if prev_author != author_id:
+            if len(papers) < 50:
+                for i in papers:
+                    if i not in simTable:
+                        simTable[i] = dict()
+                    if i % 5 != paper_mod:
+                        continue
+                    for j in papers:
+                        if i == j:
                             continue
-                        for j in papers:
-                            if i == j:
-                                continue
-                            if j not in simTable[i]:
-                                simTable[i][j] = 0
-                            simTable[i][j] = simTable[i][j] + 1
-                prev_author = author_id
-                papers = []
-            papers.append(paper_id)
-        print len(simTable)
+                        if j not in simTable[i]:
+                            simTable[i][j] = 0
+                        simTable[i][j] = simTable[i][j] + 1
+            prev_author = author_id
+            papers = []
+        papers.append(paper_id)
+    print len(simTable)
 
-    for i, rels in simTable.items():
-        for j, weight in rels.items():
-            cursor.execute("insert into papersim_author(src_id,dst_id,weight) values (%s,%s,%s);",(i, j, weight))
+for i, rels in simTable.items():
+    for j, weight in rels.items():
+        cursor.execute("insert into papersim_author(src_id,dst_id,weight) values (%s,%s,%s);",(i, j, weight))
 
 connection.commit()
 cursor.close()
