943faf50a10fe1616df011f46233972058beb450
xlvector <xlvector@gmail.com>
2011-07-22 07:51:07 +0000

    add invalid email/keywords check

--- site/signup.php
+++ site/signup.php
@@ -3,6 +3,19 @@ require_once('../api/db.php');
 $password = md5($_POST["password"]);
 $email = $_POST["email"];
 $keywords = $_POST["keywords"];
+
+if(strpos($email, "@") === false)
+{
+	echo "<h2>Email address is invalid!</h2>";
+	return;
+}
+
+if(strlen($keywords) == 0)
+{
+	echo "<h2>You must input research areas, seprated by comma</h2>";
+	return;
+}
+
 mysql_query("replace into user (email,passwd,keywords) values ('" . $email . "', '".$password."', '" . $keywords . "');");
 
 echo "SELECT id FROM user WHERE email='".$email."' and passwd = '" . $password . "'";
