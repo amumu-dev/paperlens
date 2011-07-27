a42d6ab87e4632dd6a5e8abcf66aec55bcc3861a
higuojin <higuojin@gmail.com>
2011-07-27 17:52:19 +0000

    add suggest feature add suggst including files(css & js)

---
+++ site/inc/search_bar.php
@@ -0,0 +1,11 @@
+                                <form action="search.php">
+                                        <input id="search_box" class="search_box" type="text" name="query" value=""/>
+                                        <input type="hidden" name="uid" value="<?php echo $uid; ?>"/>
+                                        <input class="search_button" type="submit" value="Search!" />
+                                <script language="javascript" type="text/javascript">
+									$("#search_box").coolautosuggest({
+									  url:"../api/search/suggest.php?key=",
+									  submitOnSelect:true
+									});
+                                </script>
+                                </form>
