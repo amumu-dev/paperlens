7cfd865c45f74778b768e39419104c71274d6eac
xlvector <xlvector@gmail.com>
2011-07-29 10:19:15 +0000

    change path

--- site/douban_oauth/doubancon.php
+++ site/douban_oauth/doubancon.php
@@ -13,7 +13,7 @@ foreach($rt_arr as $k=>$v){
 }
 //print $rt_arr['oauth_token'];
 
-$a_url = $myclient->get_authorization_url($rt_arr['oauth_token'],$rt_arr['oauth_token_secret'],"http://localhost/testphp/doubansave.php");
+$a_url = $myclient->get_authorization_url($rt_arr['oauth_token'],$rt_arr['oauth_token_secret'],"http://127.0.0.1/site/douban_oauth/doubansave.php");
 
 Header("Location: $a_url");
 
