1c065c7f5af731429e67aed0c44ff87ac084490a
xlvector <xlvector@gmail.com>
2011-08-12 02:46:49 +0000

    using paper citation relation from citeseer

--- api/recommendation/recsys_no_reason_xml.php
+++ api/recommendation/recsys_no_reason_xml.php
@@ -1,7 +1,8 @@
 <?php
 require_once("recsys.php");
 $uid = $_GET['uid'];
-$relatedTables = array("papersim_author" => 1, "default" => 0.1);
+//$relatedTables = array("papersim_author" => 1, "cite_citeseer"=>1, "default" => 0.1);
+$relatedTables = array("cite_citeseer"=>1);
 $recommendations = makingRecommendation($uid, $relatedTables);
 header('Content-Type: text/xml');
 arsort($recommendations[0]);
