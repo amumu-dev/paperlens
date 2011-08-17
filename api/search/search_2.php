7e6c468fe11cd9c80f55d3522d05b45a9150ba6a
higuojin <higuojin@gmail.com>
2011-08-17 18:22:17 +0000

    add new search api via sphinx_api

---
+++ api/search/search_2.php
@@ -0,0 +1,134 @@
+<?php
+/**
+* paperlens search api via sphinx_api
+* see also@/api/search/sphinx_api/sphinxapi.php
+*       higuojin@gmail.com
+*       07/26 2011
+*
+*/
+require_once( 'sphinx_server.php' );
+require_once('../db.php');
+
+function getAuthorName($author_id)
+{
+	$result = mysql_query('select name from author where id='.$author_id);
+	if (!$result) {
+	    return '';
+	}
+
+	while ($row = mysql_fetch_row($result))
+	{
+		$name = $row[0];
+	}
+	mysql_free_result($result);
+	return htmlspecialchars($name);
+}
+
+header('Content-Type: text/xml');
+$_DEFAULT_OFFSET=0;
+$_DEFAULT_LIMIT=10;
+
+$offset=$_DEFAULT_OFFSET;
+$limit=$_DEFAULT_LIMIT;
+if(!isset($_GET['query'])){
+        die('');
+}
+if(isset($_GET['offset'])){
+    $offset=$_GET['offset'];
+	if(!!is_int($offset)){
+		$offset=$_DEFAULT_OFFSET;
+	}
+}
+if(isset($_GET['limit'])){
+    $limit=$_GET['limit'];
+	if(!!is_int($limit)){
+		$limit=$_DEFAULT_LIMIT;
+	}
+}
+$query = $_GET['query'];
+
+$scl = getSphinxServer($offset, $limit);
+if($scl->IsConnectError()){
+	die('search server down!');
+}
+
+$scl->setFieldWeights(array('title'=>2000));
+$scl->SetMatchMode(SPH_MATCH_EXTENDED2);
+#$scl->SetRankingMode(SPH_RANK_BM25);
+$scl->SetSortMode ( SPH_SORT_EXPR,"@weight*log2(3 + citations/1000) /(2030 - year)");
+$index_name='idx1';
+$res = $scl->Query ($query, $index_name );
+
+//no results
+echo "<result>";
+if(!$res || $res['total']==0){
+	//if no results, giving some suggestions here;
+	echo "<noresults>null</noresults>";
+}
+else{
+	//highlight options
+	$opts = array(
+	'before_match' => '<span class="high">',
+	'after_match'  => '</span>',
+    'around' => 80,
+	//'limit' => 256, default 256
+	//'chunk_separator' => '..',default '...'
+	);
+	
+	$pids = $data = $title = array();
+	foreach($res['matches'] as $k=>$v){
+        $pids[] = $k;
+    }
+	$pid = implode(',',$pids);
+	$result = mysql_query("select id, title, booktitle,year,journal,abstract,type from paper where id in ($pid)");
+	while ($row = mysql_fetch_row($result))
+	{
+		$tmp_row=array();
+		if("www" == $row[6]){continue;}
+		$tmp_row['id'] = $row[0];
+		$tmp_row['title'] = $row[1];
+		$tmp_row['booktitle'] = $row[2];
+		$tmp_row['year'] = $row[3];
+		$tmp_row['journal'] = $row[4];
+		$tmp_row['abstract'] = $row[5];
+		//get author
+		$res_auth = mysql_query('select author_id from paper_author where paper_id='.$row[0].' order by rank');
+		if (!$res_auth) {
+			die('Query failed: ' . mysql_error());
+		}
+
+		while ($r = mysql_fetch_row($res_auth))
+		{
+			$tmp_row['author'][$r[0]] = getAuthorName($r[0]);
+		}
+		mysql_free_result($res_auth);
+		
+		$data[]=$tmp_row;
+		$title[]=$row[1];
+	}
+	
+	//output global results info
+    echo "<global>";
+    echo "<hits>".$res['total']."</hits>";
+    echo "</global>";
+	
+	//replace with highlights and output results
+	$title = $scl->BuildExcerpts($title,$index_name,$query,$opts);
+	foreach($data as $k=>$v){
+		echo "<paper>";
+		echo "<id>" . $data[$k]['id'] . "</id>";
+		echo "<title>" . htmlspecialchars($title[$k]) . "</title>";
+		if(strlen( $data[$k]['booktitle'] ) > 0)
+			echo "<booktitle>" . htmlspecialchars($data[$k]['booktitle']) . "</booktitle>";
+		else echo "<booktitle>" . htmlspecialchars($data[$k]['journal']) . "</booktitle>";
+		echo "<year>" . htmlspecialchars($data[$k]['year']) . "</year>";
+		echo "<abstract>" . htmlspecialchars($data[$k]['abstract'] ) . "</abstract>";
+		foreach($data[$k]['author'] as $author_id => $author_name)
+		{
+			echo "<author><id>" . $author_id. "</id><name>".$author_name."</name></author>";
+		}
+		echo "</paper>";
+	}
+}
+echo "</result>";
+?>
---
+++ api/search/sphinx_server.php
@@ -0,0 +1,19 @@
+<?
+/**
+*		sphinx_server.php
+*       return sphinx server api via sphinx_api
+*       higuojin@gmail.com
+*       08/07 2011
+*/
+require( "sphinx_api/sphinxapi.php" );
+function getSphinxServer($offset=0, $limit=15){
+	$scl = new SphinxClient();
+	$scl->SetServer('127.0.0.1',3312);
+	
+	//default mode; SPH_MATCH_ALL; 
+	//see also@http://sphinxsearch.com/wiki/doku.php?id=sphinx_manual_chinese#?È«ÎÄËÑË÷ÉèÖÃ
+	$scl->SetMatchMode(SPH_MATCH_ALL); 
+	$scl->SetLimits($pffset,$limit);
+	return $scl;
+}
+?>
