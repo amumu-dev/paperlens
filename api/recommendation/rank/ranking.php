117fa3e6716defb3a07b62ad8884b5cb9a5979f6
xlvector <xlvector@gmail.com>
2011-09-03 13:11:24 +0000

    diversity ranking by reasons

--- api/recommendation/rank/ranking.php
+++ api/recommendation/rank/ranking.php
@@ -2,6 +2,22 @@
 
 function ranking($recommendations)
 {
-	
+	$used_reasons = array();
+	foreach($recommendations[1] as $paper_id => $reasons)
+	{
+		arsort($reasons);
+		foreach($reasons as $reason_id => $weight)
+		{
+			if(!array_key_exists($reason_id, $used_reasons))
+			{
+				$used_reasons[$reason_id] = 1;
+			}
+			else $used_reasons[$reason_id]++;
+			
+			$used_count = $used_reasons[$reason_id];
+			$recommendations[0][$paper_id] /= 1 + $used_count;
+			break;
+		}
+	}
 }
 ?>
