aee9d685d19ca345ad6da08017d38c6824688452
xlvector <xlvector@gmail.com>
2011-12-07 08:33:20 +0000

    vim

--- site/reader/index.php
+++ site/reader/index.php
@@ -69,7 +69,7 @@ function IsChinese($buf)
 					if(in_array($dst_id, $load_history)) $weight *= 0.1;
 					if(!array_key_exists($dst_id, $rank)) $rank[$dst_id] = $weight;
 					else $rank[$dst_id] += $weight;
-					//echo $src_id . "&nbsp;" . $dst_id . "&nbsp;" . $weight . "<br>";
+					echo $src_id . "&nbsp;" . $dst_id . "&nbsp;" . $weight . "<br>";
 				}
 			}
 			$minvalue = 10000;
@@ -117,7 +117,7 @@ function IsChinese($buf)
 				$like_str = "<a id=\"feed_$id\" class=\"like\" $onclick_str>喜欢</a>";
 				if(in_array($id, $history)) $like_str = "<a id=\"feed_$id\" class=\"like\" $onclick_str style=\"background:#AAA;\">谢谢</a>";
 
-				echo "<div class=\"item\"><span class=\"feed\">$like_str &nbsp;<a href=\"$link\" target=_blank>$name</a></span>"
+				echo "<div class=\"item\">$id<span class=\"feed\">$like_str &nbsp;<a href=\"$link\" target=_blank>$name</a></span>"
 					. "<span class=\"article\">" .date("Y-m-d H:i:s",$pubdate) . "&nbsp;<a href=\"$article_link\" target=_blank>$article</a></span>"
 					. "<span class=\"subscribe\"><a $onclick_str href=\"http://fusion.google.com/add?feedurl=$encode_link\" target=_blank><img src=\"http://gmodules.com/ig/images/plus_google.gif\" /></a>&nbsp;"
 					. "<a $onclick_str target=\"_blank\" href=\"http://xianguo.com/subscribe?url=$encode_link\"><img src=\"http://xgres.com/static/images/sub/sub_XianGuo_09.gif\" /></a>"
