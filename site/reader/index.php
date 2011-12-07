6a6f143cdeedfbff20d8eeca2b87b249a8e5cedb
xlvector <xlvector@gmail.com>
2011-12-07 08:12:11 +0000

    vim

--- site/reader/index.php
+++ site/reader/index.php
@@ -19,13 +19,13 @@ function IsChinese($buf)
 		<script src="reader.js" type="text/javascript"></script>
 		<style type="text/css">
 			body {font-family:Verdana; font-size:13px;line-height:26px;}
-			#main{width:960px; margin:0 auto; margin-top:20px;padding-left:5px;}
-			#head{width:960px; margin:0 auto; font-size:40px;margin-top:30px; font-weight:bold;}
+			#main{width:1000px; margin:0 auto; margin-top:20px;padding-left:5px;}
+			#head{width:1000px; margin:0 auto; font-size:40px;margin-top:30px; font-weight:bold;}
 			.item {width:100%;text-align:left;clear:both;}
 			.feed {width:30%; float:left; }
-			.article {width:50%; float:left; }
+			.article {width:55%; float:left; }
 			.like{display:block;width:40px;float:left;background:#000;height:18px;line-height:18px;cursor:pointer;margin-top:3px;font-size:12px;text-align:center;color:#FFF;}
-			.subscribe {width:20%; float:left;vertical-align:bottom; }
+			.subscribe {width:15%; float:left;vertical-align:bottom; }
 			a {font-size:13px; color: #1D5261;}
 			a:hover {color: #5697A3;}
 			/*.feedtitle {height:18px;line-height:18px; display:block;float:left;width:95%;}*/
@@ -92,11 +92,11 @@ function IsChinese($buf)
 			foreach($rank as $id => $w)
 			{
 				$ids .= $id . ',';
-				if(++$n > 64) break;
+				if(++$n > 100) break;
 			}
 			$ids .= '0';
 			$n = 0;
-			$result = mysql_query("select name, link, latest_article_title, latest_article_link, id, modify_at from feeds where id in ($ids) order by modify_at desc");
+			$result = mysql_query("select name, link, latest_article_title, latest_article_link, id, modify_at from feeds where id in ($ids) and modify_at>0 order by modify_at desc");
 			while($row=mysql_fetch_array($result))
 			{
 				$name = $row[0];
@@ -112,13 +112,13 @@ function IsChinese($buf)
 				$pubdate = $row[5];
 				if(strlen($article) < 10 || strlen($article_link) > 180 || strlen($article) > 80) continue;
 				if(!IsChinese($article)) continue;
-				if(++$n > 32) break;
+				if(++$n > 24) break;
 				$onclick_str = "onclick=\"addHistory($id);\"";
 				$like_str = "<a id=\"feed_$id\" class=\"like\" $onclick_str>喜欢</a>";
 				if(in_array($id, $history)) $like_str = "<a id=\"feed_$id\" class=\"like\" $onclick_str style=\"background:#AAA;\">谢谢</a>";
 
 				echo "<div class=\"item\"><span class=\"feed\">$like_str &nbsp;<a href=\"$link\" target=_blank>$name</a></span>"
-					. "<span class=\"article\">" .date("Y-M-d",$pubdate) . "<a href=\"$article_link\" target=_blank>$article</a></span>"
+					. "<span class=\"article\">" .date("Y-m-d H:i:s",$pubdate) . "<a href=\"$article_link\" target=_blank>$article</a></span>"
 					. "<span class=\"subscribe\"><a $onclick_str href=\"http://fusion.google.com/add?feedurl=$encode_link\" target=_blank><img src=\"http://gmodules.com/ig/images/plus_google.gif\" /></a>&nbsp;"
 					. "<a $onclick_str target=\"_blank\" href=\"http://xianguo.com/subscribe?url=$encode_link\"><img src=\"http://xgres.com/static/images/sub/sub_XianGuo_09.gif\" /></a>"
 					. "</div>";
