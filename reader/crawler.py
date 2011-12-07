14fba66547c47cca07a8fc44175c3a909a9772a9
xlvector <xlvector@gmail.com>
2011-12-07 06:32:53 +0000

    vim

--- reader/crawler.py
+++ reader/crawler.py
@@ -11,7 +11,7 @@ class Crawler:
         try:
             req = urllib2.Request(url)
             req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.107 Safari/535.1')
-            fp = urllib2.urlopen(req, timeout=10)
+            fp = urllib2.urlopen(req, timeout=150)
             text = ''
             while 1:
                 s = fp.read()
