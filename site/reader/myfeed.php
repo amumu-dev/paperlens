7e3c7ca412a7380fd1eb4aaf3bf11dbc798ef3af
xlvector <xlvector@gmail.com>
2011-12-08 03:25:01 +0000

    vim

--- site/reader/myfeed.php
+++ site/reader/myfeed.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-type: text/xml");
 require_once('db.php');
 $user_id = $_GET['uid'];
 $result = mysql_query("select feed_id from myfeed where user_id=$user_id;");
@@ -9,6 +10,14 @@ while($row = mysql_fetch_array($result))
 	$feeds .= $feed_id . ',';
 }
 $feeds .= '0';
+?>
+<rss version="2.0">
+  <channel>
+    <title>我的阅读列表</title>
+    <description>个性化阅读列表</description>
+    <link>http://www.feedforall.com/industry-solutions.htm</link>
+
+<?php
 $result = mysql_query("select a.article_id from feed_articles a, articles b where a.article_id = b.id and a.feed_id in ($feeds) order by b.pub_at desc limit 20");
 while($row = mysql_fetch_array($result))
 {
@@ -17,3 +26,4 @@ while($row = mysql_fetch_array($result))
 	echo file_get_contents("http://www.reculike.com/site/reader/articles/" . (string)($article_id % 10) . "/" . (string)($article_id));
 }
 ?>
+</channel></rss>
